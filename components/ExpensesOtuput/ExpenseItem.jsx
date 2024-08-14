3023d80f9bbe1aed4b76e00e84a7d583adc68715
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-08-14 14:12:24 +0200

    add navigation from useNavigate and navigation

--- components/ExpensesOtuput/ExpenseItem.jsx
+++ components/ExpensesOtuput/ExpenseItem.jsx
@@ -1,10 +1,17 @@
 import { Pressable, Text, View, StyleSheet } from 'react-native';
 import { GlobalStyles } from '../../constants/styles';
 import { getFormattedDate } from '../../utils/data';
+import { useNavigation } from '@react-navigation/native';
 
 export default function ExpenseItem({ description, amount, date }) {
+  const navigation = useNavigation();
+
+  function expensePressHandler() {
+    navigation.navigate('ManageExpense');
+  }
+
   return (
-    <Pressable>
+    <Pressable onPress={expensePressHandler} style={({ pressed }) => pressed && styles.pressed}>
       <View style={styles.expenseItem}>
         <View>
           <Text style={[styles.textBase, styles.description]}>{description}</Text>
@@ -19,6 +26,9 @@ export default function ExpenseItem({ description, amount, date }) {
 }
 
 const styles = StyleSheet.create({
+  pressed: {
+    opacity: 0.75,
+  },
   expenseItem: {
     padding: 12,
     marginVertical: 8,
