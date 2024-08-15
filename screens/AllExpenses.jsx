e18a49dc76382b58ceda3f69e7b6feee18fba1f2
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-08-15 14:33:57 +0200

    update cod with context api feature

--- screens/AllExpenses.jsx
+++ screens/AllExpenses.jsx
@@ -1,5 +1,8 @@
+import { useContext } from 'react';
+import { ExpensesContext } from '../store/expenses-context';
 import ExpensesOutput from '../components/ExpensesOtuput/ExpensesOutput';
 
 export default function AllExpenses() {
-  return <ExpensesOutput expensesPeriod="Total" />;
+  const expensesCtx = useContext(ExpensesContext);
+  return <ExpensesOutput expenses={expensesCtx.expenses} expensesPeriod="Total" fallbackText="No registered expenses found!" />;
 }
--- screens/ManageExpense.jsx
+++ screens/ManageExpense.jsx
@@ -1,10 +1,12 @@
-import { useLayoutEffect } from 'react';
+import { useContext, useLayoutEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
 import { GlobalStyles } from '../constants/styles';
 import IconButton from '../components/ui/IconButton';
 import Button from '../components/ui/Button';
+import { ExpensesContext } from '../store/expenses-context';
 
 export default function ManageExpense({ route, navigation }) {
+  const expensesCtx = useContext(ExpensesContext);
   const editedExpenseId = route.params?.expenseId;
   const isEditing = !!editedExpenseId;
 
@@ -15,6 +17,7 @@ export default function ManageExpense({ route, navigation }) {
   }, [navigation, isEditing]);
 
   function deleteExpenseHandler() {
+    expensesCtx.deleteExpense(editedExpenseId);
     navigation.goBack();
   }
 
@@ -23,6 +26,19 @@ export default function ManageExpense({ route, navigation }) {
   }
 
   function confirmHandler() {
+    if (isEditing) {
+      expensesCtx.updateExpense(editedExpenseId, {
+        description: 'Test!!!!',
+        amount: 29.99,
+        date: new Date('2024-08-14'),
+      });
+    } else {
+      expensesCtx.addExpense({
+        description: 'Test',
+        amount: 19.99,
+        date: new Date('2024-08-12'),
+      });
+    }
     navigation.goBack();
   }
 
